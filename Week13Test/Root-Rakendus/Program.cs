54262dec4920c9cfd1881f06d7ed482e51ff8451
Opilane TTHK <opilane@tthk.ee>
2021-11-24 09:57:57 +0200

    Test

---
+++ Week13Test/Root-Rakendus/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Root_Rakendus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\opilane\Samples\";
+
+            Console.WriteLine("Enter directory  name:");
+            string kasutajaKaust = Console.ReadLine();
+
+            string newDirectoryFullPath = $@"{rootDirectory}\{kasutajaKaust}";
+            bool directoryExists = Directory.Exists(newDirectoryFullPath);
+
+            if (directoryExists)
+            {
+                Console.WriteLine($"{kasutajaKaust} already  exists in Samples.");
+            }
+            else
+            {
+                Directory.CreateDirectory(newDirectoryFullPath);
+                Console.WriteLine($"Directory {kasutajaKaust} has been created.");
+            }
+
+            Directory.CreateDirectory(newDirectoryFullPath);
+
+
+        }
+    }
+}
